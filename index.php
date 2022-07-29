e9d8df4703412409efed6077b4b4320635b08d36
Yoshinori-Fukusumi <cakdq701@gmail.com>
2022-07-29 10:30:01 +0900

    ペットショップ2課題完了

--- index.php
+++ index.php
@@ -2,18 +2,38 @@
 require_once __DIR__ . '/functions.php';
 
 $lists = display_list();
+$keyword = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+    $keyword = filter_input(INPUT_GET, 'keyword');
+    $keyword_param = '%' . $keyword . '%';
+    $lists = search_list($keyword_param);
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
-<link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
 <style>
-    body { padding: 20px;}
+    body {
+        padding: 20px;
+    }
+
+    p {
+        margin: 0;
+    }
 </style>
+
 <body>
 
     <h2>本日のご紹介ペット！</h2>
+
+    <form action="" method="get">
+        <span>キーワード:</span>
+        <input type="search" name="keyword" placeholder="キーワードの入力">
+        <input type="submit" value="検索"><br>
+    </form>
 
     <?php foreach ($lists as $list) : ?>
         <br>
@@ -21,7 +41,8 @@ $lists = display_list();
         <p><?= h($list['description']) ?></p>
         <p><?= h($list['birthday']) . ' 生まれ' ?></p>
         <p><?= '出身地 ' . h($list['birthplace']) ?></p>
-        <br><hr>
+        <br>
+        <hr>
     <?php endforeach; ?>
 
 
